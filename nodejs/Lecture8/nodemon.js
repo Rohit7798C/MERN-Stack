f5205cdc7721675a3071db2fd0febe611fb03f2e
Rohit Rajkumar Chauhan <114377886+Rohit7798C@users.noreply.github.com>
2024-06-19 18:05:38 +0530

    Add files via upload

---
+++ nodejs/Lecture8/nodemon.js
@@ -0,0 +1,18 @@
+// 1) Everytime we make changes to the file, we have to save and run the file from the terminal
+// 2) for that we install Nodemon that helps in refreshing the changes to the file without running the file again & again. 
+
+// 3) nodemon is a tool that helps develop Node.js based applications by automatically restarting the node application when file changes in the directory are detected.
+// 4) Nodemon Installation : npm install -g nodemon
+// 5) -g indicates that the package we are installing is accessible throughout the file
+
+// 6) For using the nodemon we use command nodemon http_module.js
+// 7) But in my case it is not working , so I have used npx nodemon http_module.js (And it worked!).
+
+
+
+
+
+
+
+
+
