80bed2d2451cff7f61a7733d0c84ecbb51830059
Rohit Rajkumar Chauhan <114377886+Rohit7798C@users.noreply.github.com>
2024-06-30 15:37:06 +0530

    Add files via upload

---
+++ expressjs/Lecture14/main.js
@@ -0,0 +1,28 @@
+// First of all we have to create a package.json file 
+// command : npm init
+
+// Install the npm package
+// command : npm install
+
+// Install the express-generator
+// command: npm install express-generator
+
+// Display the command options with -h option
+// command: express -h
+
+// Creating a Dummy app using the View Engine as : Pug
+// command: express --view=pug myapp
+
+// Here we are creating the app using the File Engine : EJS
+// command: express --view=ejs myapp
+
+// This command creates the Directory named ejsapp
+// This Directory contains multiple folders
+
+// If a folder named node_modules is absent then we have to install the http-errors module
+// command: npm install http-errors
+
+// If we want to run the file app.js for the app we use the command 
+// command: set DEBUG=ejsapp:* & npm start
+
+// We can make changes to the app and run with the above command
